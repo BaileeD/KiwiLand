074cb0442ff1e00e841976cea3105a60f8e65e82
Eion11 <Richards.stt@gmail.com>
2017-04-08 15:52:34 +1200

    Graphics for Tiles [2] (First one wasnt added)

---
+++ src/nz/ac/aut/ense701/gameModel/MoveDirection.java
@@ -0,0 +1,12 @@
+package nz.ac.aut.ense701.gameModel;
+
+/**
+ * Enumeration class MoveDirection - describes the direction of a move
+ * 
+ * @author AS
+ * @version August 2011
+ */
+public enum MoveDirection
+{
+	NORTH, EAST, SOUTH, WEST;
+}
