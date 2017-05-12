9985918f4a978ec744a574974fb7b2c082a4001b
chaitanya2017 <chaitvarma@gmail.com>
2017-05-12 18:53:01 +1200

    Added new test cases
    Added new test cases to delete a game save and update an existing game save

--- test/nz/ac/aut/ense701/gameModel/GameSavesTest.java
+++ test/nz/ac/aut/ense701/gameModel/GameSavesTest.java
@@ -79,4 +79,37 @@ public class GameSavesTest {
         GameSave gameSave = GameSave.getLastGameSave("");
         assertNull(gameSave);
     }
+    
+    /**
+     * Test of updateGame method, of class GameSave with new level id
+     */
+    @Test
+    public void testSaveOver() {
+        GameSave gameSave = new GameSave();
+        gameSave.setPlayerName("John Doe");
+        gameSave.setSaveName("ThunderBird");
+        gameSave.setLevel(1);
+        gameSave.setSaveDate(new Date());
+        boolean save = gameSave.save();
+        assertTrue(save);
+        
+        ArrayList<GameSave> gameSaves = GameSave.getPlayerGameSaves("John Doe");
+        assertEquals(gameSaves.get(0).getLevel(), 1);
+        
+        boolean status = GameSave.updateGame(gameSaves.get(0).getGameSaveId(), 3, new Date());
+        assertTrue(status);
+    }
+    
+    
+    /**
+     * Test of deleteSave method, of class GameSave 
+     */
+    @Test
+    public void testDeleteGameSave() {
+        ArrayList<GameSave> gameSaves = GameSave.getPlayerGameSaves("John Doe");
+        boolean status = GameSave.deleteSave(gameSaves.get(0).getGameSaveId());
+        assertTrue(status);
+    }
+    
+    
 }
